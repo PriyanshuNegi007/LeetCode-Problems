bd8fd71681ed95d83d6ab121bc6f7b1924f76dd2
Priyanshu Negi <pnegi2703@gmail.com>
2025-07-11 17:57:32 +0530

    Create 22 Generate Parenthesis.java

---
+++ BackTracking/22 Generate Parenthesis.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        helper(0,0,0, "", result,n);
+        return result;
+    }
+
+    public void helper(int opening, int closing, int index, String output, List<String> result, int n){
+        if(index == n*2){  // Base Case
+            result.add(output);
+            return;
+        }
+        //rule 1 opening < n   (Can i place an opening)
+        if(opening < n){
+            helper(opening + 1, closing,index + 1, output + "(", result, n);
+        }
+
+        //rule 2 closing < opening 
+        if(closing < opening){
+            helper(opening, closing + 1, index + 1, output + ")", result, n);
+        }
+    }
+}
