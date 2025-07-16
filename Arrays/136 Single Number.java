6f198960579c961687a9468184fa4d552e7d25cf
Priyanshu Negi <pnegi2703@gmail.com>
2025-07-16 15:27:51 +0530

    Create 136 Single Number.java

---
+++ Arrays/136 Single Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumberBitsApproach(int[] nums) {
+       int Xor= 0;
+
+       for(int i=0;i<nums.length;i++){
+        Xor = Xor ^ nums[i];
+       }
+
+       return Xor;
+    }
+}
