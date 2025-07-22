60612da0d48ab92a29193a2f2b272e507cf7b1af
Priyanshu Negi <pnegi2703@gmail.com>
2025-07-22 14:58:15 +0530

    Create 160 Intersection of 2 LinkedList.java

---
+++ Linked List/160 Intersection of 2 LinkedList.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //Use set as it does not allow duplicates
+        Set<ListNode> set = new HashSet<>();
+        while(headB != null){
+            set.add(headB);
+            headB = headB.next;
+        }
+
+        while(headA != null){
+            if(set.contains(headA)){
+                return headA;
+            }
+            headA = headA.next;
+        }
+        return null;
+    }
+}
