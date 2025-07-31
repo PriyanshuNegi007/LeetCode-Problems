3499657d12eb4ea2d74899fb84fb2377606602c9
Priyanshu Negi <pnegi2703@gmail.com>
2025-07-31 11:48:32 +0530

    Create 622 Design Circular Queue.java

---
+++ Queue/622 Design Circular Queue.java
@@ -0,0 +1,56 @@
+class MyCircularQueue {
+    int[] array;
+    int front;
+    int rear;
+    int size;
+    int count;
+
+    public MyCircularQueue(int k) {
+        size = k;
+        array = new int[k];
+        front = 0;
+        rear = 0;
+        count = 0;
+    }
+    
+    public boolean enQueue(int value) {
+        if(isFull()){
+            return false;
+        }
+        array[rear] = value;
+        rear = (rear + 1) % size;
+        count++;
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if(isEmpty()){
+            return false;
+        }
+        front = (front + 1) % size;
+        count--;
+        return true;
+    }
+    
+    public int Front() {
+        if(isEmpty()){
+            return -1;
+        }
+        return array[front];
+    }
+    
+    public int Rear() {
+        if(isEmpty()){
+            return -1;
+        }
+        return array[(rear -1 + size) % size];
+    }
+    
+    public boolean isEmpty() {
+        return count == 0;
+    }
+    
+    public boolean isFull() {
+        return count == size;
+    }
+}
