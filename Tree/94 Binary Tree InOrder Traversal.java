6dc69ec68bf3ca234cdb26f6b2767c94b155c6f6
Priyanshu Negi <pnegi2703@gmail.com>
2025-07-31 17:25:37 +0530

    Update 94 Binary Tree InOrder Traversal.java

--- Tree/94 Binary Tree InOrder Traversal.java
+++ Tree/94 Binary Tree InOrder Traversal.java
@@ -1,3 +1,39 @@
+class Solution {
+    void inorderTraversalRecursion(TreeNode root,List<Integer> result){
+        if(root==null){
+            return;
+        }
+        inorderTraversalRecursion(root.left,result);
+        result.add(root.val);
+        inorderTraversalRecursion(root.right,result);
+    }
+
+    public List<Integer> inOrderTraversalIteration(TreeNode root){
+        List<Integer> list=new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode currentNode = root;
+
+        while(currentNode != null || !stack.isEmpty()){
+            while(currentNode != null){
+                stack.push(currentNode);
+                currentNode = currentNode.left;
+            }
+            currentNode = stack.pop();
+            list.add(currentNode.val);
+            currentNode = currentNode.right; //Explore right
+        }
+        return list;
+    }
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result=new ArrayList<>();
+        //inorderTraversalRecursion(root,result);
+        //return result;
+        return inOrderTraversalIteration(root);
+    }
+}
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,20 +49,3 @@
  *     }
  * }
  */
-class Solution {
-    public void inorderTraversal(TreeNode root,List<Integer> result){
-        if(root==null){
-            return;
-        }
-        inorderTraversal(root.left,result);
-        result.add(root.val);
-        inorderTraversal(root.right,result);
-    }
-
-
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result=new ArrayList<>();
-        inorderTraversal(root,result);
-        return result;
-    }
-}
