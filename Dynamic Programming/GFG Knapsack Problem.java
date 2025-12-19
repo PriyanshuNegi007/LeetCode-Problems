7933a9973ca0fbea458b9b0127b05ae353a9ed39
Priyanshu Negi <pnegi2703@gmail.com>
2025-12-19 20:09:24 +0530

    Update GFG Knapsack Problem.java

--- Dynamic Programming/GFG Knapsack Problem.java
+++ Dynamic Programming/GFG Knapsack Problem.java
@@ -1,9 +1,16 @@
 class Solution {
-    public int knapsack(int W, int val[], int wt[]) {
-        // code here
-        return helper(W, val, wt, 0, val.length); // 0 is index 
+    static int dp[][];
+    
+    public static int knapsack(int W, int val[], int wt[]) {
+        dp = new int[val.length + 1][W + 1];
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[i].length; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return helper(W, val, wt, 0, val.length); // 0 is index
     }
-    // Brute Force
+    
     static int helper(int W, int val[], int wt[], int currentIndex, int length){
         //Base Case
         if(currentIndex == length){
@@ -13,6 +20,11 @@ class Solution {
         if(W == 0){
             return 0;
         }
+        // IF already in cache then don't compute it
+        if(dp[currentIndex][W] != -1){
+            return dp[currentIndex][W];
+        }
+    
         // Leave it
         int valueAfterLeave = helper(W, val, wt, currentIndex + 1, length);
         
@@ -21,8 +33,8 @@ class Solution {
         if(wt[currentIndex] <= W){
             valueAfterTake = val[currentIndex] + helper(W - wt[currentIndex], val, wt, currentIndex + 1, length);
         }
-        
         //Check which gives max value
-        return Math.max(valueAfterLeave, valueAfterTake);
+        dp[currentIndex][W] =  Math.max(valueAfterLeave, valueAfterTake);
+        return dp[currentIndex][W];
     }
 }
