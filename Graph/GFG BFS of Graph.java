9bcbbba204397fb5b3e9a41b6d8350ec686911e1
Priyanshu Negi <pnegi2703@gmail.com>
2025-12-23 20:56:40 +0530

    Create GFG BFS of Graph.java

---
+++ Graph/GFG BFS of Graph.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        final int V = adj.size();
+        boolean visited [] = new boolean[V];
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
+        queue.offer(0);
+        visited[0] = true;
+        
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            result.add(current);
+            
+            for(int neighbour: adj.get(current)){
+                if(!visited[neighbour]){
+                    queue.offer(neighbour);
+                    visited[neighbour] = true;
+                }
+            }
+        } 
+        return result;        
+    }
+}
